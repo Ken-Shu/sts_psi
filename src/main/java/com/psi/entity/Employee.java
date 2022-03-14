6308cc4d51b899db7fd84e2b7a76e88b122929e6
User <User@DESKTOP-QI9V5ON>
2022-03-14 21:30:34 +0800

    spring boot _psi 2022-03-12

---
+++ src/main/java/com/psi/entity/Employee.java
@@ -0,0 +1,54 @@
+package com.psi.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "employees")
+public class Employee {	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column
+	@Size(min = 2, max = 50, message = "{employee.name.size}")
+	private String name;
+	
+	@ManyToOne
+	@JoinColumn(name = "depatment_id") //外鍵約束	
+	private Department department;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+	
+	
+}
---
+++ src/main/java/com/psi/entity/Purchase.java
@@ -0,0 +1,82 @@
+package com.psi.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.FutureOrPresent;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@Entity
+@Table(name = "purchases")
+public class Purchase {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
+	private Long id;
+	
+	//加上日期不可小於今日的限制
+	@Column
+	@Temporal(TemporalType.DATE)
+	@FutureOrPresent(message = "{purchase.date.futureOrPresent}")
+	@NotNull(message = "{purchase.date.notnull}")
+	@JsonFormat(pattern = "yyyy-MM-dd")
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date date = new Date();
+	
+	@ManyToOne
+	@JoinColumn(name = "supplier_id") //外鍵
+	private Supplier supplier;
+	
+	@ManyToOne
+	@JoinColumn(name = "employee_id") //外鍵
+	private Employee employee;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public Supplier getSupplier() {
+		return supplier;
+	}
+
+	public void setSupplier(Supplier supplier) {
+		this.supplier = supplier;
+	}
+
+	public Employee getEmployee() {
+		return employee;
+	}
+
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
+	}
+	
+	
+	
+}
